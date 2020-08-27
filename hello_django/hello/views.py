eed34c717166dc95f0b9ca60e1f76f501410223f
Damian Pieczykolan / Art3usz <Damian.Pieczykolan@10g.pl>
2020-08-27 13:05:00 +0200

    day 2 django, starting with templates

--- hello_django/hello/views.py
+++ hello_django/hello/views.py
@@ -1,7 +1,20 @@
 #from django.shortcuts import render
-
-# Create your views here.
+import re
+from datetime import datetime
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
     return HttpResponse("Hello World, Django!")
+
+def hello_there(request,name):
+   
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
